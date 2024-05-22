84a6ad16a9c1de13415dbb095334607772636ae0
Alex <alex@polyform.co>
2024-05-22 11:08:27 +0200

    sliders labels

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        if sender == redSlider {
+            redValueLabel.text = formatSliderValue(sender.value)
+            } else if sender == greenSlider {
+                greenValueLabel.text = formatSliderValue(sender.value)
+            } else if sender == blueSlider {
+                blueValueLabel.text = formatSliderValue(sender.value)
+            }
+    }
+    
+    private func formatSliderValue(_ value: Float) -> String {
+        return value.formatted(.number.precision(.fractionLength(2)))
+    }
 }
 
